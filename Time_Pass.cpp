46c8521d8839a1d256ec1f03d25e391bfe788693
Anurag Singh <annuu033@hotmail.com>
2020-08-17 04:20:13 +0530

    Add project files.

---
+++ Time_Pass.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+  int i;
+
+  printf("/** This is also possible in C++ **/\n");
+
+
+  cout << "Hello World " << endl;
+  cin.get();
+
+}
